4d15712df2cedb21281edd63c678c19003171d36
cherithr <cherith.rachabattuni@gmail.com>
2026-06-01 23:39:47 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -15,7 +15,6 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const analytics = getAnalytics(app)
