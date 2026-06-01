9961981ea56f2d2a61f60e9a3238feb074348ec9
cherithr <cherith.rachabattuni@gmail.com>
2026-06-01 23:34:50 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA1ttMSCQ_zh8bb4S5Yly8ROJFOV48NuIQ",
-  authDomain: "taxops-elite.firebaseapp.com",
-  projectId: "taxops-elite",
-  storageBucket: "taxops-elite.firebasestorage.app",
-  messagingSenderId: "1029832152458",
-  appId: "1:1029832152458:web:6083824672cfdabd552473",
-  measurementId: "G-X459N6N3XH"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
